482d0e57f413d377f5fa97af957f77044011e8e7
藤原未来 <www.motimoti12@gmail.com>
2019-12-04 15:52:14 +0900

    カルーセルjs導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,26 @@
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
+
+$(document).on("turbolinks:load", function() {
+  $('.toppage-slider__wrapper').slick({
+  // ドットにクラスを追加
+    dotsClass: 'slide-dots',
+    // ドットを追加する要素を指定
+    appendDots: $('.toppage-slider'),
+    // スライダー矢印の指定
+    prevArrow:'<i class="fas fa-chevron-left", id="prev-arrow"></i>',
+    nextArrow:'<i class="fas fa-chevron-right", id="next-arrow"></i>',
+    // ドットの有無 インジケーター
+    dots: true,
+    // 自動再生
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 800,
+});
+
+// ドットの操作
+$('.slide-dots li').on('mouseover', function() {
+  $('.slider').slick('goTo', $(this).index());
+});
+});
