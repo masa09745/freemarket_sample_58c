e523d4bc2fa3bcdd308b05dcf811e3111440be46
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-12-21 17:49:16 +0900

    仮置きルーティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,7 @@ class UsersController < ApplicationController
 
   def profile
   end
+
+  def identification
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   scope :mypage do
     get 'users/profile'
     get 'users/card'
-    resources :users
+    get 'users/identification'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
