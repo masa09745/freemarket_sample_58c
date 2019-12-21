a0ae942041aaa2900f79afd0ea16afe46d0f7734
putn <clooler.sc@gmail.com>
2019-12-21 16:02:10 +0900

    commited

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :freemarket
   scope :mypage do
     get 'users/profile'
+    get 'users/card'
     resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
