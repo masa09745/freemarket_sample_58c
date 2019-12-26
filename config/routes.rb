02482d78d1b80ec623f792662c5e021ddee0810e
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-12-26 20:15:37 +0900

    SNS認証完全版(DB反映込み)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: "freemarket#index"
     get 'freemarket/show'
     get 'freemarket/create'
