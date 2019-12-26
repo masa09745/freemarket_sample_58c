045e9fc341f16b32c21f79f9766504cd7aeba34a
AtsuhiMatsuo <cool.and.thefunk@me.com>
2019-12-26 22:34:11 +0900

    link修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     get 'users/identification'
     get 'users/logout'
     resources 'users'
-    get 'registrations/registration'
+  
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
