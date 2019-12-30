8601a0cff87742610f2c152aafebda27f44a7be4
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2019-12-30 13:54:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,8 @@ set :linked_files, fetch(:linked_files, []).push("config/master.key")
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
 
   # desc 'upload secrets.yml'
