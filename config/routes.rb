487a4ef661425e2c3ed340689ca175186c304427
putn <clooler.sc@gmail.com>
2020-01-05 17:33:57 +0900

    --fix f9a08d03f17d9f794c4edd5024ef469265b09d0a

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
     get 'freemarket/create'
   scope :mypage do
     get 'users/profile'
-    get 'users/card'
     get 'users/identification'
     get 'users/logout'
-    resources 'users'
+    resources :users
+    get 'cards/create'
+    resources :cards
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
