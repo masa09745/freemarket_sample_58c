e93fca015c25e8ce34a53a9f261b21c4979c10f9
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-11 23:00:16 +0900

    カテゴリーデータインプット

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
     get 'cards/create'
     resources :cards
   end
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
