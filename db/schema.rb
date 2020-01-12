0e464d7daaf0bbed8a5a29bfb5d514e8a92c1cb6
AtsuhiMatsuo <cool.and.thefunk@me.com>
2020-01-12 23:20:30 +0900

    db drop

--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,6 @@ ActiveRecord::Schema.define(version: 2019_12_25_120842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["mobile"], name: "index_users_on_mobile"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
