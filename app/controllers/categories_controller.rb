cd70ca9ec1712ee73cf1a40b5d4d28469799183a
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-13 21:57:42 +0900

    続カテゴリー追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,12 @@
 class CategoriesController < ApplicationController
   def index
     @parents = Category.all.order("id ASC").limit(13)
+    @ladys = Category.where(ancestry: 1)
+    @mens = Category.where(ancestry: 2)
+    @kids = Category.where(ancestry: 3)
+  end
+
+  def show
+    @category  = Category.find(params[:id])
   end
 end
