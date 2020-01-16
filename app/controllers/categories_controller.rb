3a213fcb63a970cd7b5e2dbea85e8babddecebbd
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-16 13:24:33 +0900

    インスタンス変数定義(仮)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,13 @@ class CategoriesController < ApplicationController
     @lifes = Category.where(ancestry: 4)
     @books = Category.where(ancestry: 5)
     @hobbies = Category.where(ancestry: 6)
+    @cosmes = Category.where(ancestry: 7)
+    @cameras = Category.where(ancestry: 8)
+    @sports = Category.where(ancestry: 9)
+    @handmades = Category.where(ancestry: 10)
+    @tickets = Category.where(ancestry: 11)
+    @cars = Category.where(ancestry: 12)
+    @others = Category.where(ancestry:13)
   end
 
   def show
