65d8896d1a6049ecd05b76d08fba1e698041da8f
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-17 20:29:07 +0900

    コントローラーアクションをすっきりと

--- app/controllers/freemarket_controller.rb
+++ app/controllers/freemarket_controller.rb
@@ -1,11 +1,11 @@
 class FreemarketController < ApplicationController
   
   def index
-    @parents = Category.all.order("id ASC").limit(13)
+    
   end
 
   def show
-    @parents = Category.all.order("id ASC").limit(13)
+
   end
 
   def create
