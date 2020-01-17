3d3db796146a2b9d6b43410d334ee84267d0a8b1
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-17 19:03:28 +0900

    カテゴリーのトップページの記述変更

--- app/controllers/freemarket_controller.rb
+++ app/controllers/freemarket_controller.rb
@@ -1,10 +1,13 @@
-class CategoriesController < ApplicationController
+class FreemarketController < ApplicationController
+  
   def index
     @parents = Category.all.order("id ASC").limit(13)
   end
 
   def show
     @parents = Category.all.order("id ASC").limit(13)
-    @category  = Category.find(params[:id])
+  end
+
+  def create
   end
 end
--- app/models/freemarket.rb
+++ app/models/freemarket.rb
@@ -6,4 +6,6 @@ class Freemarket < ApplicationRecord
   # has_many :orders
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
+
+  @parents = Category.all.order("id ASC").limit(13)
 end
