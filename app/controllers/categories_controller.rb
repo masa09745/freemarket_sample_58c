b1adb6c41833981f151a0abdfe16ea3f6a60c0da
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-17 20:29:07 +0900

    コントローラーアクションをすっきりと

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,9 @@
 class CategoriesController < ApplicationController
   def index
-    @parents = Category.all.order("id ASC").limit(13)
+    
   end
 
   def show
-    @parents = Category.all.order("id ASC").limit(13)
     @category  = Category.find(params[:id])
   end
 end
