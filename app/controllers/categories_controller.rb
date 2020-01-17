d5a74268f6138da602b35074908a4d00e96f7f24
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-17 19:03:28 +0900

    カテゴリーのトップページの記述変更

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @parents = Category.all.order("id ASC").limit(13)
     @category  = Category.find(params[:id])
     @children = Category.find(params[:id]).children
     @parent = Category.find(params[:id])
