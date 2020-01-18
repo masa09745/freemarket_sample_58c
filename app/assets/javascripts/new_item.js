0578ce7b54605375c52b3048badae280f96fd96c
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-01-18 18:23:03 +0900

    コミット

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -23,24 +23,51 @@ $(document).on("turbolinks:load", function() {
 
         fileReader.readAsDataURL(file);
 
-        fileReader.onloadend = function () {
-          var src = fileReader.result;
-          var html = `<div class='item-image' data-image="${file.name}">
-                        <div class=' item-image__content'>
-                          <div class='item-image__content--icon'>
-                            <img src=${src} width="114" height="80" >
-                          </div>
-                        </div>
-                        <div class='item-image__operetion'>
-                          <div class='item-image__operetion--delete'>削除</div>
-                        </div>
-                      </div>`
-          $('.sell-upload__image').prepend(html);
-        };
-
         var input_area = $('.sell-upload__image');
         var new_input = $(`<input id="image-file" type="file" style="display:none" name="freemarket[item_images_attributes][${countA}][image_url]">`);
         input_area.prepend(new_input);
+
+        if(countA == 10) {
+          $("#image-drop").css("display", "none");
+          $('#image-drop2').show();
+        };
+
+        if(countA < 11) {
+          fileReader.onloadend = function () {
+            var src = fileReader.result;
+            var html = `<div class='item-image' data-image="${file.name}">
+                          <div class=' item-image__content'>
+                            <div class='item-image__content--icon'>
+                              <img src=${src} width="114" height="80" >
+                            </div>
+                          </div>
+                          <div class='item-image__operetion'>
+                            <div class='item-image__operetion--delete'>削除</div>
+                          </div>
+                        </div>`
+            $('.sell-upload__image').prepend(html);
+          };
+        } else {
+          fileReader.onloadend = function () {
+            var src = fileReader.result;
+            var html = `<div class='item-image' data-image="${file.name}">
+                          <div class=' item-image__content'>
+                            <div class='item-image__content--icon'>
+                              <img src=${src} width="114" height="80" >
+                            </div>
+                          </div>
+                          <div class='item-image__operetion'>
+                            <div class='item-image__operetion--delete'>削除</div>
+                          </div>
+                        </div>`
+            $('.sell-upload__image2').prepend(html);
+          };
+        };
+
+        if (countA == 16) {
+          $("#image-drop2").css("display", "none");
+        };
+
 
       });
     });
