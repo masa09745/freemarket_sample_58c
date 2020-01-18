56bd8ceb4f0d673e49de3264fc7d3b199b577dda
藤原未来 <www.motimoti12@gmail.com>
2020-01-18 18:12:21 +0900

    修正

--- app/models/freemarket.rb
+++ app/models/freemarket.rb
@@ -1,8 +1,8 @@
 class Freemarket < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :category
-  # belongs_to :size
-  # belongs_to :brand
-  # has_many :orders
+  belongs_to :user
+  belongs_to :category
+  belongs_to :size
+  belongs_to :brand
+  has_many :orders
   has_many :item_images, dependent: :destroy
 end
