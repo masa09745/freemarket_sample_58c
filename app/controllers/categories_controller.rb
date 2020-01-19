2b005f84f01b3e0b93f66942daecd44a5f37d24b
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-19 21:29:58 +0900

    categories/showにインスタンス変数を定義

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category  = Category.find(params[:id])
+    @children = Category.find(params[:id]).children
+    @parent = Category.find(params[:id])
   end
 end
