5dc3224483ea9547b5dda0a3822d4ec68233f532
AtsuhiMatsuo <cool.and.thefunk@me.com>
2020-01-20 19:54:41 +0900

    コメントアウト削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,16 +18,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.build_deliver_address 
 
     session[:deliver_address] = @user.deliver_address
-    # session[:nickname] = params[:session][:nickname]
-    # session[:email] = params[:session][:email]
-    # session[:password] = params[:session][:password]
-    # session[:user_last_name] = params[:session][:user_last_name]
-    # session[:user_first_name] = params[:session][:user_first_name]
-    # session[:user_last_name_kana] = params[:session][:user_last_name_kana]
-    # session[:user_first_name_kana] = params[:session][:user_first_name_kana]
-    # session[:birth_year] = params[:session][:birth_year]
-    # session[:birth_month] = params[:session][:birth_month]
-    # session[:birth_day] = params[:session][:birth_day]
+    
   end
     
   def credit
@@ -36,16 +27,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     session[:user]["first_name"]       =   user_params[:first_name]
     session[:user]["last_name_kana"]   =   user_params[:last_name_kana]
     session[:user]["first_name_kana"]  =   user_params[:first_name_kana]
-    # アドレスデータをセッションに格納
+    
     session[:deliver_address] = user_params[:deliver_address_attributes]
-    # session[:last_name] = params[:session][:last_name]
-    # session[:first_name] = params[:session][:first_name]
-    # session[:last_name_kana] = params[:session][:last_name_kana]
-    # session[:first_name_kana] = params[:session][:first_name_kana]
-    # session[:postal_code] = params[:session][:postal_code]
-    # session[:prefecture] = params[:session][:prefecture]
-    # session[:city] = params[:session][:city]
-    # session[:street_address] = params[:session][:street_address]
+    
   end
   
   def phone
@@ -75,34 +59,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       card_id:        @customer.default_card,
     )
     
-    # @user = User.new(
-    #   nickname: session[:nickname],
-    #   email: session[:email],
-    #   password: session[:password],
-    #   user_last_name: session[:user_last_name],
-    #   user_first_name: session[:user_first_name],
-    #   user_last_name_kana: session[:user_last_name_kana],
-    #   user_first_name_kana: session[:user_first_name_kana],
-    #   birth_year: session[:birth_year],
-    #   birth_month: session[:birth_month],
-    #   birth_day: session[:birth_day],
-      
-    # )
-    # @user.deliver_address = @user.build_deliver_address(
-    #   last_name: session[:last_name],
-    #   first_name: session[:first_name],
-    #   last_name_kana: session[:last_name_kana],
-    #   first_name_kana: session[:first_name_kana],
-    #   postal_code: session[:postal_code],
-    #   prefecture: session[:prefecture],
-    #   city: session[:city],
-    #   street_address: session[:street_address]
-    # )
 
-    # @user.save
-    # @user.deliver_address.save
-    
-    #  binding.pry
      
     if @user.save
       user = @user
