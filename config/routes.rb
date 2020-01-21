17afbabc1e68147e99d1e378e877c70727a93f93
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-21 14:13:47 +0900

    アクションの明記

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
 
   resources 'freemarkets'
 
-  resources 'categories'
+  resources 'categories', only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
