800ab8b81164e265fe7eda99274746e807f4933e
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-01-25 15:15:28 +0900

    パンくず追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,10 +7,32 @@ crumb :users do
   parent :root
 end
 
+crumb :profiles do
+  link 'プロフィール', profile_users_path
+  parent :users
+end
+
+crumb :cards do
+  link '支払い方法', cards_path
+  parent :users
+end
+
+crumb :logout do
+  link 'ログアウト', logout_users_path
+  parent :users
+end
+
+crumb :newcard do
+  link 'クレジットカード情報入力' ,new_card_path
+  parent :cards
+end
+
+
 crumb :categories do
   link 'カテゴリー一覧', categories_path
   parent :root
 end
+
 
 crumb :category do |category|
   link category.category_name, category_path(category)
