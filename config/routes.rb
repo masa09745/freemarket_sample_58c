999670b13e4e65a6bb11ff3279e3cd3e387dff78
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-01-29 19:59:57 +0900

    ユーザー名表示

--- config/routes.rb
+++ config/routes.rb
@@ -45,9 +45,8 @@ Rails.application.routes.draw do
     post "sign_in/complete" => "users/registrations#complete"
   end  
 
-  resources 'freemarkets'
-
   resources 'categories', only: [:index, :show]
+  get 'user/show' => "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
