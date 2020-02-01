f440a259fbd8606927bf607a0a45986fa4aac666
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-02-01 17:33:52 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_01_19_121323) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
+    t.string "prefecture"
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_121323) do
 
   create_table "deliver_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.string "prefecture_id", null: false
+    t.string "prefecture", null: false
     t.string "city", null: false
     t.string "street_address", null: false
     t.string "building_name"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_121323) do
     t.integer "price", null: false
     t.string "condition", null: false
     t.string "ship_charge", null: false
-    t.integer "ship_from", null: false
+    t.string "ship_from", null: false
     t.string "ship_day", null: false
     t.text "description", null: false
     t.integer "status", default: 1, null: false
