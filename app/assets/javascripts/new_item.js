d3255fe658388f494ccccb61d325912c20cd4505
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-02-05 21:30:31 +0900

    出品画像編集終了

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -1,8 +1,5 @@
 $(document).on('turbolinks:load', function(){
-  $(function(){
-    $('#image-drop').on('click',function () {
-      $('.hidden-image').trigger('click');
-    });
+
 
     var id = $('.item-preview__inner').length;
     console.log(id)
@@ -28,8 +25,6 @@ $(document).on('turbolinks:load', function(){
     //items/:i/editページへリンクした際のアクション=======================================
     if (window.location.href.match(/\/freemarkets\/\d+\/edit/)){
       var id = $('.item-preview__inner').length;
-      console.log(id)
-
       //登録済み画像のプレビュー表示欄の要素を取得する
       var prevContent = $('#image-drop').prev();
       labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
@@ -93,60 +88,23 @@ $(document).on('turbolinks:load', function(){
     });
 
 
-    $(document).on('click', '.item-edit__delete', function() {
-      var count = $('.item-preview').length;
-      setLabel(count);
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      $(`#item-preview__${id}`).remove();
-      $(`#freemarket_item_images_attributes_${id}_image_url`).val("");
-      var count = $('.item-preview').length;
-      if (count == 4) {
-        $('#image-drop').show();
-      }
-      setLabel(count);
 
-      if(id < 5){
-        $('.label-box').attr({id: `label-box--${id}`,for: `freemarket_item_images_attributes_${id}_image_url`});
-      }
-    });
 
-    // 画像の削除
     $(document).on('click', '.item-edit__delete', function() {
       var count = $('.item-preview__inner').length;
       setLabel(count);
       var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      $(`#item-preview__inner__${id}`).remove();
-    //新規登録時と編集時の場合分け==========================================================
-      //新規投稿時
-      //削除用チェックボックスの有無で判定
-      if ($(`#freemarket_item_images_attributes_${id}__destroy`).length == 0) {
-            //フォームの中身を削除 
-        $(`#freemarket_item_images_attributes_${id}_image_url`).val("");
-        var count = ('.item-preview__inner').length;
-            //5個めが消されたらラベルを表示
-          if (count == 4) {
-            $('#image-drop').show();
-          }
-        setLabel(count);
-          if(id < 5){
-            $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-            }
-          } 
-        else {
-          //投稿編集時
-          $(`#freemarket_item_images_attributes_${id}__destroy`).prop('checked',true);
-          //5個めが消されたらラベルを表示
-          if (count == 4) {
-            $('#image-drop').show();
-          }
-            //ラベルのwidth操作
-          setLabel();
-          //ラベルのidとforの値を変更
-          //削除したプレビューのidによって、ラベルのidを変更する
-          if(id < 5){
-            $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-          }
-        }
+      $(`#freemarket_item_images_attributes_${id}__destroy`).prop('checked',true);
+      $(`#item-preview__${id}`).remove();
+      $(`#freemarket_item_images_attributes_${id}_image_url`).val("");
+      var count = $('.item-preview__inner').length;
+      if (count == 4) {
+        $('#image-drop').show();
+      }
+      setLabel(count);
+      if(count < 5){
+        $('.label-box').attr({id: `label-box--${id}`,for: `freemarket_item_images_attributes_${id}_image_url`});
+      }
     });
   });
-});
+
