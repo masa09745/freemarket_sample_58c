55b40e2c01d72b2abade4fe6f316eadbb83a1774
putn <clooler.sc@gmail.com>
2020-02-08 03:32:35 +0900

    feat(購入機能): freemarketsコントローラーの追記と修正

--- app/controllers/freemarkets_controller.rb
+++ app/controllers/freemarkets_controller.rb
@@ -1,5 +1,5 @@
 class FreemarketsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :buy, :exhibit]
+  before_action :authenticate_user!, only: [:new, :buy]
 
   def index
     @freemarkets = Freemarket.where(status: "1").limit(10).order('created_at ASC')
@@ -13,39 +13,6 @@ class FreemarketsController < ApplicationController
   def new
     @freemarket = Freemarket.new
     @freemarket.item_images.build
-  end
-
-  def edit
-    @freemarket = Freemarket.find(params[:id])
-  end
-
-  def update
-    @freemarket = Freemarket.find(params[:id])
-      if @freemarket.user_id == current_user.id
-        @freemarket.update(freemarket_edit_params)
-        redirect_to  list_users_path
-      end
-  end
-
-  def destroy
-    @freemarket = find_freemarket_by_id
-    @freemarket.destroy
-    redirect_to list_users_path
-    flash[:notice] = '削除しました'
-  end
-
-  def exhibit
-    @freemarket = Freemarket.includes(:user).find(params[:id])
-    prefecture = Prefecture.all
-  end
-
-  def buy
-    @card = Card.find_by(user_id: current_user.id)
-    if @card.present?
-      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @card_info = customer.cards.retrieve(@card.card_id)
-    end
   end
 
   def create
@@ -63,15 +30,76 @@ class FreemarketsController < ApplicationController
     end
   end
 
+  def edit
+    @freemarket = Freemarket.find(params[:id])
+  end
 
+  def update
+    @freemarket = Freemarket.find(params[:id])
+      if @freemarket.user_id == current_user.id
+        @freemarket.update(freemarket_edit_params)
+        redirect_to  list_users_path
+      end
+  end
+
+  def destroy
+    @item = find_freemarket_by_id
+    @item.destroy
+    redirect_to list_users_path
+    flash[:notice] = '削除しました'
+  end
+
+  def buy
+    @user = User.find_by(id: current_user.id)
+    @deliver = DeliverAddress.find_by(user_id: current_user.id)
+    @item = find_freemarket_by_id
+    @card = find_card_by_id
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to freemarket_path(@item)
+    elsif @card.present?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_info = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def pay
+    @deliver = DeliverAddress.find_by(user_id: current_user.id)
+    @item = find_freemarket_by_id
+    @card = find_card_by_id
+    begin
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      Payjp::Charge.create(
+      amount: @item.price,
+      customer: @card.customer_id,
+      currency: 'jpy',
+      )
+      @item.update(status: 2)
+      Order.create(
+        user_id: current_user.id,
+        freemarket_id: @item.id, 
+        deliver_address_id: @deliver.id,
+      )
+      redirect_to purchased_users_path
+      flash[:notice] = '購入が完了しました'
+    rescue Payjp::PayjpError => e
+      redirect_to buy_freemarket_path(@item)
+      flash[:alert] = 'このカードはご利用になれません。ご利用のクレジットカード会社へお問い合わせください。'
+    end
+  end
 
   private
+
   def freemarket_params
     params.require(:freemarket).permit(:item, :description, :price, :condition, :ship_charge, :ship_from, :ship_day, item_images_attributes: [:image_url]).merge(user_id: current_user.id)
   end
 
   def freemarket_edit_params
     params.require(:freemarket).permit(:item, :description, :price, :condition, :ship_charge, :ship_from, :ship_day, item_images_attributes: [:id, :image_url, :_destroy]).merge(user_id: current_user.id)
+  end
+
+  def find_card_by_id
+    Card.find_by(user_id: current_user.id)
   end
 
   def find_freemarket_by_id
