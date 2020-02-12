0afcc97fea2a5fa8b82346612035899c35c4f197
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2020-02-12 17:31:13 +0900

    readme更新と使ってないアソシエーション削除

--- app/models/freemarket.rb
+++ app/models/freemarket.rb
@@ -2,9 +2,6 @@ class Freemarket < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
-  # belongs_to :category
-  # belongs_to :size
-  # belongs_to :brand
   has_many :orders
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
--- db/migrate/20200119121323_add_new_to_freemarkets.rb
+++ db/migrate/20200119121323_add_new_to_freemarkets.rb
@@ -1,8 +1,5 @@
 class AddNewToFreemarkets < ActiveRecord::Migration[5.2]
   def change
     add_reference :freemarkets, :user, null: false, foreign_key: true
-    # add_reference :freemarkets, :category, null: false, foreign_key: true
-    # add_reference :freemarkets, :size, null: false, foreign_key: true
-    # add_reference :freemarkets, :brand, null: false, foreign_key: true
   end
 end
