24b8aedf7961797054d5804329c4ed1c18160520
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2021-05-13 22:59:34 +0900

    rails 5.2.4→6.1.3 & ruby 2.5.3→2.7.1 update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,7 +20,6 @@ module FreemarketSample58c
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in your application. config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
